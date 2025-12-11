2def2205b50c6e8a15b38754b90a760e1f715a12
Zyprush18 <zyprush9@gmail.com>
2025-12-12 03:13:04 +0800

    day_1

---
+++ index.py
@@ -0,0 +1,93 @@
+# variable
+# hallo = "eko"
+# age  = 9
+# address = "jl.banteng"
+# isDeath = False
+
+# print(hallo)
+
+# print("hello " + hallo)
+
+# print("Hello nama sya "+ hallo + "umur saya " + str(age) + " alamat saya " + address + " dan apakah saya sudah mati: "+ str(isDeath))
+
+# print(f"Hello nama saya {hallo}, umur saya {age}, alamat saya {address}, dan apakah saya sudah mati?: {isDeath}")
+
+# print("""   haofjbjkiedbfjjkf
+
+# kjekfkjekjflkejlkfjerlkfe
+# fnekwnfknelkenflkeg
+# fejefohwefhiwejfpow
+# ffnkwelsjgp;slfs
+# efwfwefwefd
+
+#     paragraf kedua,,kata saya "kakkaka"
+# """)
+# print('''   haofjbjkiedbfjjkf
+# kjekfkjekjflkejlkfjerlkfe
+# fnekwnfknelkenflkeg
+# fejefohwefhiwejfpow
+# ffnkwelsjgp;slfs
+# efwfwefwefd
+
+#     paragraf kedua,,kata saya "kakkaka"
+# ''')
+
+# print("kata saya \"jjdj\" \n kkkk \tkkk zul\bpp \'")
+# karin baik
+
+# nama = "Zaim"
+
+Nama = "Zaim"
+Umur = "18"
+Alamat = "pongo"
+Status = "Mahasiswa"
+Sudah_Menikah = False
+print("="*50 + " Punya Zaim "+ "="*50)
+print(f" nama saya {Nama}, umur saya {Umur}, alamat saya {Alamat}, saya seorang {Status}, dan apakah sudah menikah {Sudah_Menikah}")
+
+
+
+# zaim
+
+
+
+
+
+
+# komentar
+"""
+    mamang
+    budi
+    eko
+    sinta
+    edo
+"""
+
+'''
+    ejjoidejijieojroiej
+    jeojoeorpoerpojepojr
+'''
+# peliharaanku itu kucing  
+peliharaan = "kucing"
+
+
+# tipe data
+# operator
+# if
+# for sama while
+# function
+# range
+# array
+# iterator
+
+
+# karin
+nama = "karin"
+umur = 18
+tinggi = 150.1
+mahasiswa = True
+hobi = "main"
+
+print("="*50 + " Punya Karin "+ "="*50)
+print (f"nama saya {nama}, umur saya {umur} ✅")
+
